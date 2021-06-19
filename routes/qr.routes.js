92c32571b7eb7107eb1edc5d7ef6003ed3db137d
Igor Rybyakov <rybyakovigor@gmail.com>
2021-06-19 22:04:57 +0500

    config linter, prettier

--- routes/qr.routes.js
+++ routes/qr.routes.js
@@ -1,16 +1,17 @@
-const { Router } = require("express");
-const useragent = require("express-useragent");
+const { Router } = require('express');
+const useragent = require('express-useragent');
+
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const source = req.headers["user-agent"];
+    const source = req.headers['user-agent'];
     const ua = useragent.parse(source);
-    if (ua.platform === "Android") {
-      res.redirect("https://play.google.com/store");
+    if (ua.platform === 'Android') {
+      res.redirect('https://play.google.com/store');
     }
-    if (ua.platform === "iPhone") {
-      res.redirect("https://www.apple.com/ru/app-store/");
+    if (ua.platform === 'iPhone') {
+      res.redirect('https://www.apple.com/ru/app-store/');
     }
     res.end(
       `<a style="font-size:30px" href='http://vk.com'>App for Android</a>
@@ -18,7 +19,7 @@ router.get("/", async (req, res) => {
       <a style="font-size:30px" href='http://facebook.com'>App for IOS</a>`
     );
   } catch (e) {
-    res.status(500).json({ message: "Something went wrong, try again." });
+    res.status(500).json({ message: 'Something went wrong, try again.' });
   }
 });
 
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 
-app.use("/api/qr", require("./routes/qr.routes"));
+app.use('/api/qr', require('./routes/qr.routes'));
 
 const PORT = 5000;
 async function start() {
-  app.listen(PORT, () =>
-    console.log(`App has been started at port ${PORT}...`)
-  );
+  // eslint-disable-next-line no-console
+  app.listen(PORT, () => console.log(`App has been started at port ${PORT}...`));
 }
 
 start();
