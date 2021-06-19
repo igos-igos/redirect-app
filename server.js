a556525484080213e89c2faab9d8ae757a8e2d9b
Igor Rybyakov <rybyakovigor@gmail.com>
2021-06-19 21:51:58 +0500

    style

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.json({extended: true}))
+app.use(express.json({ extended: true }));
 
-app.use("/api/qr", require("./routes/qr.routes"))
+app.use("/api/qr", require("./routes/qr.routes"));
 
-const PORT = 5000
+const PORT = 5000;
 async function start() {
-    app.listen(PORT, () => console.log(`App has been started at port ${PORT}...`))
+  app.listen(PORT, () =>
+    console.log(`App has been started at port ${PORT}...`)
+  );
 }
 
-start()
+start();
