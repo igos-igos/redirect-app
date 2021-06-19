36f805d31de20bec925250aa19ebd7a0c4d31e32
Igor Rybyakov <rybyakovigor@gmail.com>
2021-06-19 21:28:52 +0500

    create server + redirect route

---
+++ routes/qr.routes.js
@@ -0,0 +1,25 @@
+const { Router } = require("express")
+const useragent = require("express-useragent")
+const router = Router()
+
+router.get("/", async (req, res) => {
+  try {
+    const source = req.headers["user-agent"]
+    const ua = useragent.parse(source)
+    if (ua.platform === "Android") {
+      res.redirect("http://vk.com")
+    }
+    if (ua.platform === "iPhone") {
+      res.redirect("http://facebook.com")
+    }
+    res.end(
+      `<a style="font-size:30px" href='http://vk.com'>App for Android</a>
+      <br> <br>
+      <a style="font-size:30px" href='http://facebook.com'>App for IOS</a>`
+    )
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong, try again."})
+  }
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json({extended: true}))
+
+app.use("/api/qr", require("./routes/qr.routes"))
+
+const PORT = 5000
+async function start() {
+    app.listen(PORT, () => console.log(`App has been started at port ${PORT}...`))
+}
+
+start()
